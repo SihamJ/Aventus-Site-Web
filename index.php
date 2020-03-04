be1ef1822f669b228e908da5b69a5a6b8e57c072
JANATI SIHAM <siham.janati@etu.unistra.fr>
2020-03-04 17:27:10 +0100

    ajout de paragraphe qui sommes nous

--- index.php
+++ index.php
@@ -15,7 +15,11 @@
 
 <article class="about">
 <h2> Qui Sommes Nous ? </h2>
-<p>-Article-</p>
+<p>-Littering a dark and dreary road lay the past relics of browser-specific tags, incompatible DOMs, broken CSS support, and abandoned browsers.
+
+We must clear the mind of the past. Web enlightenment has been achieved thanks to the tireless efforts of folk like the W3C, WASP, and the major browser creators.
+
+The CSS Zen Garden invites you to relax and meditate on the important lessons of the masters. Begin to see with clarity. Learn to use the time-honored techniques in new and invigorating fashion. Become one with the web.</p>
 </article>
 
 <section class="moments">
@@ -30,4 +34,4 @@
 	include 'templates/footer.php';
 ?>
 </body>
-</html> 
+</html>
